84a208ce2a214ecc7eb21948116ff1443ee939bc
R20eem <reeme.2911425@gamil.com>
2026-02-23 23:10:34 +0000

    chore: align Kotlin plugins and detekt config

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
-	kotlin("jvm") version "2.2.21"
-	kotlin("plugin.spring") version "2.2.21"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
 	id("org.springframework.boot") version "4.0.3"
 	id("io.spring.dependency-management") version "1.1.7"
-	kotlin("plugin.jpa") version "2.2.21"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 group = "com.envdashboard"
@@ -54,3 +55,8 @@ allOpen {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+}
+
